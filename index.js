b2d1d19d8b2af98fc5fb95f0d6bfd506a7d80783
christopher <coronac511@gmail.com>
2021-03-14 23:33:43 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,61 @@
+number=20;
+
+function add() {
+  return number += 5;
+}
+
+
+add();
+
+function subtract() {
+    return number -= 1;
+  }
+
+  subtract();
+
+
+  
+function multiply() {
+    return number *= 1;
+  }
+
+  multiply();
+
+  
+function divide() {
+    return number /= 2;
+  }
+
+  divide();
+ 
+function add(a,b) {
+return a+b;
+}
+
+function subtract(a,b) {
+  return a-b;
+}
+
+function multiply(a,b) {
+  return a*b;
+}
+
+function divide(a,b) {
+  return a/b;
+}
+
+function increment(n) {
+  return n+=1;
+}
+
+function decrement(n) {
+  return n-=1;
+}
+
+function makeInt(string) {
+  return parseInt(string,10)
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string)
+}
